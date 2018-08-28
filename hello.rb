f4ad54b7c8429b94349ddd834bbf4855975db435
Satoko Ishikawa <satoko@steinfluss.net>
2018-08-28 23:36:12 +0900

    初めてのruby ファイルを追加

---
+++ hello.rb
@@ -0,0 +1,13 @@
+print "Hello,Ruby\n"
+a = 1+1
+print a
+print "\n"
+a = "Hello, World"
+puts a
+
+puts "-----"
+
+name = "Hiro"
+puts (name + " " + "Nice to meet you")
+
+puts ("Hello, My name is" + " " + name )
