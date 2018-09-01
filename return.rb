0d5fc822ec348a4c295813cb5c7dda79406fef76
Satoko Ishikawa <satoko@steinfluss.net>
2018-09-02 00:23:03 +0900

    戻り値の追加

---
+++ return.rb
@@ -0,0 +1,18 @@
+
+def add(num1, num2)
+    result = num1 + num2
+    return result # 戻り値
+end
+
+result1 = add(1, 2)
+puts result1
+
+result2 = add(2, 4)
+puts result2
+
+result_final = add(result1, result2)
+puts result_final
+
+
+
+
