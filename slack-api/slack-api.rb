49a92815291f065a61d76722a0fef3b5bdd9b735
Satoko Ishikawa <satoko@steinfluss.net>
2018-09-06 23:35:09 +0900

    引数名を「text」から「user」に修正

--- slack-api/slack-api.rb
+++ slack-api/slack-api.rb
@@ -7,7 +7,7 @@ params: {
     token: ENV["SLACK_TOKEN"],
     channel: "#random",
     text: "Hiro, I have a question.",
-    as_text: true
+    as_user: true
 })
 
 puts JSON.pretty_generate(JSON.parse(response))
