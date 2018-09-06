dd003125ae18a7130e40b3bbb7de66bd5e0dfadb
Satoko Ishikawa <satoko@steinfluss.net>
2018-09-06 23:28:04 +0900

    SLACKにメッセージをポストするように編集

--- slack-api/slack-api.rb
+++ slack-api/slack-api.rb
@@ -2,10 +2,12 @@
 require "http"
 require "json"
 
-response = HTTP.post("https://slack.com/api/auth.test",
+response = HTTP.post("https://slack.com/api/chat.postMessage",
 params: {
-    token: ENV["SLACK_TOKEN"]
-
+    token: ENV["SLACK_TOKEN"],
+    channel: "#random",
+    text: "Hiro, I have a question.",
+    as_text: true
 })
 
 puts JSON.pretty_generate(JSON.parse(response))
